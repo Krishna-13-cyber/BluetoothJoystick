7d530a6b0cc7debbbbfea35468895180d2c634d2
Krishna-13-cyber <84722531+Krishna-13-cyber@users.noreply.github.com>
2021-10-12 00:50:48 +0530

    Add files via upload
    This is the latest code (printing three ADC values),...GPIO 36-SW..GPIO 39-Vrx....GPIO 34-Vry.

--- main.c
+++ main.c
@@ -8,35 +8,26 @@ void adc_task(void *arg)
     adc1_config_width(ADC_WIDTH_BIT_12);
     adc1_config_channel_atten(ADC1_CHANNEL_0,ADC_ATTEN_DB_11); 
     int val = adc1_get_raw(ADC1_CHANNEL_0);
-	while(1)
-	{	
-        val = adc1_get_raw(ADC1_CHANNEL_0);
-        printf("%d\n", val);
-        vTaskDelay(10 / portTICK_PERIOD_MS);  
-    }
 	adc1_config_width(ADC_WIDTH_BIT_12);
-    
     adc1_config_channel_atten(ADC1_CHANNEL_3,ADC_ATTEN_DB_11); 
     int val1 = adc1_get_raw(ADC1_CHANNEL_3);
-	while(1)
-	{	
-        val1 = adc1_get_raw(ADC1_CHANNEL_3);
-        printf("%d\n", val1);
-        vTaskDelay(10 / portTICK_PERIOD_MS);  
-    }
-adc1_config_width(ADC_WIDTH_BIT_12);
-   
-    adc1_config_channel_atten(ADC1_CHANNEL_6,ADC_ATTEN_DB_11); 
+    adc1_config_width(ADC_WIDTH_BIT_12);
+adc1_config_channel_atten(ADC1_CHANNEL_6,ADC_ATTEN_DB_11); 
     int val2 = adc1_get_raw(ADC1_CHANNEL_6);
-	while(1)
+    while(1)
 	{	
-        val2 = adc1_get_raw(ADC1_CHANNEL_6);
-        printf("%d\n", val2);
+       val = adc1_get_raw(ADC1_CHANNEL_0);
+       val1 = adc1_get_raw(ADC1_CHANNEL_3);
+       val2 = adc1_get_raw(ADC1_CHANNEL_6);
+     printf("Value1 : %d ",  val);
+      printf("Value2 : %d ",  val1);
+       printf("Value3 : %d ",  val2);
+      printf("\n");
+     // printf("%d\n","%d\n","%d\n",val,val1,val2);
         vTaskDelay(10 / portTICK_PERIOD_MS);  
     }
 }
 void app_main(void)
 {
-    xTaskCreate(&adc_task,"adc_task",4096,NULL,1,NULL);
+    xTaskCreate(&adc_task,"adc_task",2048,NULL,1,NULL);
 }
-

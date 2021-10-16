3a7de272ac93b04407bdedb42740b1d6a1869d03
Krishna-13-cyber <krishnanarayanan132002@gmail.com>
2021-10-16 23:17:52 +0530

    Updated One

--- main/main.c
+++ main/main.c
@@ -5,9 +5,9 @@
 
 void adc_task(void *arg)
 {
-    adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(ADC1_CHANNEL_0,ADC_ATTEN_DB_11); 
-    int val = adc1_get_raw(ADC1_CHANNEL_0);
+  //  adc1_config_width(ADC_WIDTH_BIT_12);
+   // adc1_config_channel_atten(ADC1_CHANNEL_0,ADC_ATTEN_DB_11); 
+    //int val = adc1_get_raw(ADC1_CHANNEL_0);
 	adc1_config_width(ADC_WIDTH_BIT_12);
     adc1_config_channel_atten(ADC1_CHANNEL_3,ADC_ATTEN_DB_11); 
     int val1 = adc1_get_raw(ADC1_CHANNEL_3);
@@ -16,14 +16,28 @@ adc1_config_channel_atten(ADC1_CHANNEL_6,ADC_ATTEN_DB_11);
     int val2 = adc1_get_raw(ADC1_CHANNEL_6);
     while(1)
 	{	
-       val = adc1_get_raw(ADC1_CHANNEL_0);
+     //  val = adc1_get_raw(ADC1_CHANNEL_0);
        val1 = adc1_get_raw(ADC1_CHANNEL_3);
        val2 = adc1_get_raw(ADC1_CHANNEL_6);
-     printf("Value1 : %d ",  val);
-      printf("Value2 : %d ",  val1);
-       printf("Value3 : %d ",  val2);
-      printf("\n");
-     // printf("%d\n","%d\n","%d\n",val,val1,val2);
+     //printf("Value : %d ",  val);
+      if(val1>=0&&val1<=1000)
+       {
+	printf("Forwards");//printf("Value1 : %d ",  val1);
+	printf("Value : %d ",val1);
+	}
+       
+      else if(val1>=4000&&val1<=4095)
+       {
+	printf("Backwards");  //printf("Value2 : %d ",  val2);
+	printf("Value : %d ",val1);
+	} 
+	else
+       {
+	printf("No movement"); 
+	printf("Value : %d ",val1); 
+	}
+	      printf("\n");
+     // printf("%d\n","%d\n",val1,val2);
         vTaskDelay(10 / portTICK_PERIOD_MS);  
     }
 }
